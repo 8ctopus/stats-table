d531424367b9c240e8aff294bc207c8366c3ec27
pixrr <pierry2310@gmail.com>
2021-12-07 11:36:01 +0100

    feat - explicitly apply string type to the cell value

--- lib/IgraalOSL/StatsTable/Dumper/Excel/ExcelDumper.php
+++ lib/IgraalOSL/StatsTable/Dumper/Excel/ExcelDumper.php
@@ -6,6 +6,7 @@ use IgraalOSL\StatsTable\Dumper\Dumper;
 use IgraalOSL\StatsTable\Dumper\Format;
 use IgraalOSL\StatsTable\StatsTable;
 use PhpOffice\PhpSpreadsheet\Cell\Coordinate;
+use PhpOffice\PhpSpreadsheet\Cell\DataType;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Style\Border;
@@ -322,8 +323,15 @@ class ExcelDumper extends Dumper
                 break;
         }
 
-        $sheet->setCellValueByColumnAndRow($col, $row, $value);
         $sheet->duplicateStyle($style, Coordinate::stringFromColumnIndex($col).$row);
+
+        switch ($format) {
+            case Format::STRING:
+                $sheet->setCellValueExplicitByColumnAndRow($col, $row, $value, DataType::TYPE_STRING);
+            default:
+                $sheet->setCellValueByColumnAndRow($col, $row, $value);
+                break;
+        }
     }
 
     public function getMimeType()
