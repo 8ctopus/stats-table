0b685bd26032b42547eaa71ca26f1ca8e39b746b
8ctopus <hello@octopuslabs.io>
2025-01-09 10:05:48 +0400

    Fix phpunit 12 deprecation

--- tests/StatsColumnBuilderTest.php
+++ tests/StatsColumnBuilderTest.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use IgraalOSL\StatsTable\Aggregation\AggregationInterface;
+use IgraalOSL\StatsTable\Aggregation\CountAggregation;
 use IgraalOSL\StatsTable\StatsColumnBuilder;
 use PHPUnit\Framework\TestCase;
 
@@ -10,15 +11,15 @@ class StatsColumnBuilderTest extends TestCase
 {
     public function testCreation()
     {
-        $aggregationMock = $this->getAggregationMock();
+        $aggregation = new CountAggregation('Hits');
 
         $values = [3, 5];
-        $column = new StatsColumnBuilder($values, 'Hits', 'format', $aggregationMock);
+        $column = new StatsColumnBuilder($values, 'Hits', 'format', $aggregation);
 
         $this->assertEquals($values, $column->getValues());
         $this->assertEquals('Hits', $column->getHeaderName());
         $this->assertEquals('format', $column->getFormat());
-        $this->assertEquals($aggregationMock, $column->getAggregation());
+        $this->assertEquals($aggregation, $column->getAggregation());
     }
 
     public function testEnsureIndexExists()
@@ -35,8 +36,6 @@ class StatsColumnBuilderTest extends TestCase
 
     public function testSetters()
     {
-        $aggregationMock = $this->getAggregationMock();
-
         $values = [3, 5];
         $column = new StatsColumnBuilder($values, 'Hits');
 
@@ -45,17 +44,8 @@ class StatsColumnBuilderTest extends TestCase
 
         $this->assertNull($column->getAggregation());
 
-        $column->setAggregation($aggregationMock);
-        $this->assertEquals($aggregationMock, $column->getAggregation());
-    }
-
-    /**
-     * @return AggregationInterface
-     */
-    private function getAggregationMock()
-    {
-        return $this
-            ->getMockBuilder(AggregationInterface::class)
-            ->getMockForAbstractClass();
+        $aggregation = new CountAggregation('Hits');
+        $column->setAggregation($aggregation);
+        $this->assertEquals($aggregation, $column->getAggregation());
     }
 }
