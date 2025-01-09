0bf370015fbd7e1252d81cd93621eb05850ca3ea
8ctopus <hello@octopuslabs.io>
2025-01-09 17:25:01 +0400

    Fix demo Improve demo

--- demo.php
+++ demo.php
@@ -1,46 +1,76 @@
 <?php
 
-declare(strict_types=1);
-
+use Oct8pus\StatsTable\Aggregation\AverageAggregation;
 use Oct8pus\StatsTable\Aggregation\SumAggregation;
+use Oct8pus\StatsTable\Dumper\TXT\TXTDumper;
 use Oct8pus\StatsTable\Dumper\Format;
-use Oct8pus\StatsTable\Dumper\HTML\HTMLDumper;
-use Oct8pus\StatsTable\Dumper\Text\TextDumper;
-use Oct8pus\StatsTable\StatsTable;
+use Oct8pus\StatsTable\DynamicColumn\CallbackColumnBuilder;
+use Oct8pus\StatsTable\StatsTableBuilder;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
 $data = [
     [
         'name' => 'Pierre',
-        'age' => '32',
+        'age' => 32,
+        'weight' => 100,
+        'height' => 1.87,
     ], [
         'name' => 'Jacques',
-        'age' => '28',
+        'age' => 28,
+        'weight' => 60,
+        'height' => 1.67,
     ], [
         'name' => 'Jean',
-        'age' => '32',
+        'age' => 32,
+        'weight' => 80,
+        'height' => 1.98,
     ], [
         'name' => 'Paul',
-        'age' => '25',
+        'age' => 25,
+        'weight' => 75,
+        'height' => 1.82,
     ],
 ];
 
 $headers = [
     'name' => 'Name',
     'age' => 'Age',
+    'weight' => 'Weight',
+    'height' => 'Height',
 ];
 
 $formats = [
+    'name' => Format::STRING,
     'age' => Format::INTEGER,
+    'weight' => Format::FLOAT2,
+    'height' => Format::FLOAT2,
 ];
 
 $aggregations = [
     'age' => new SumAggregation('age', Format::INTEGER),
+    'height' => new AverageAggregation('height', Format::FLOAT2),
 ];
 
-$statsTable = new StatsTable($data, $headers, $formats, $aggregations);
+$aggregationsFormats = [
+    'age' => Format::INTEGER,
+    'height' => Format::FLOAT2,
+];
 
-//$dumper = new TextDumper();
-$dumper = new HTMLDumper();
-echo $dumper->dump($statsTable);
+$builder = new StatsTableBuilder($data, $headers, $formats, $aggregations);
+
+$dynamicColumn = new CallbackColumnBuilder(function($row) : float {
+    return $row['weight'] / ($row['height'] * $row['height']);
+});
+
+$builder->addDynamicColumn('BMI', $dynamicColumn, 'BMI', Format::FLOAT2);
+
+$table = $builder->build();
+
+$table->sortMultipleColumn([
+    'age' => true,
+    'height' => true,
+]);
+
+$dumper = new TXTDumper();
+echo $dumper->dump($table);
