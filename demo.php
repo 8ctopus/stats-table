f714a288783e2802e7dc440034f986de340e4b00
8ctopus <hello@octopuslabs.io>
2025-01-09 09:22:39 +0400

    Add demo

---
+++ demo.php
@@ -0,0 +1,45 @@
+<?php
+
+use IgraalOSL\StatsTable\Aggregation\SumAggregation;
+use IgraalOSL\StatsTable\Dumper\Format;
+use IgraalOSL\StatsTable\Dumper\HTML\HTMLDumper;
+use IgraalOSL\StatsTable\Dumper\JSON\JSONDumper;
+use IgraalOSL\StatsTable\Dumper\Text\TextDumper;
+use IgraalOSL\StatsTable\StatsTable;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$data = [
+    [
+        'name' => 'Pierre',
+        'age' => '32',
+    ], [
+        'name' => 'Jacques',
+        'age' => '28',
+    ], [
+        'name' => 'Jean',
+        'age' => '32',
+    ], [
+        'name' => 'Paul',
+        'age' => '25',
+    ],
+];
+
+$headers = [
+    'name' => 'Name',
+    'age' => 'Age',
+];
+
+$formats = [
+    'age' => Format::INTEGER,
+];
+
+$aggregations = [
+    'age' => new SumAggregation('age', Format::INTEGER),
+];
+
+$statsTable = new StatsTable($data, $headers, $formats, $aggregations);
+
+//$dumper = new TextDumper();
+$dumper = new HTMLDumper();
+echo $dumper->dump($statsTable);
