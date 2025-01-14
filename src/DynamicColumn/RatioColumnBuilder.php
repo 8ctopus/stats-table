a1d8331fb0e9449bde6d3430ca8d1e0b15771d14
8ctopus <hello@octopuslabs.io>
2025-01-14 15:59:37 +0400

    Cleanup ratio column builder

--- src/DynamicColumn/RatioColumnBuilder.php
+++ src/DynamicColumn/RatioColumnBuilder.php
@@ -8,27 +8,27 @@ use Oct8pus\StatsTable\StatsTableBuilder;
 
 class RatioColumnBuilder implements DynamicColumnBuilderInterface
 {
-    private string $valueInternalName;
-    private string $overInternalName;
+    private string $numerator;
+    private string $denominator;
     private mixed $defaultValue;
 
     /**
-     * @param string $valueInternalName The small value
-     * @param string $overInternalName  The big value
-     * @param mixed  $defaultValue      Default value if big value is null
+     * @param string $numerator
+     * @param string $denominator
+     * @param mixed  $defaultValue      Default value if denominator is null
      */
-    public function __construct(string $valueInternalName, string $overInternalName, mixed $defaultValue)
+    public function __construct(string $numerator, string $denominator, mixed $defaultValue)
     {
-        $this->overInternalName = $overInternalName;
-        $this->valueInternalName = $valueInternalName;
+        $this->denominator = $denominator;
+        $this->numerator = $numerator;
         $this->defaultValue = $defaultValue;
     }
 
     public function buildColumnValues(StatsTableBuilder $statsTable) : array
     {
         $column = [];
-        $values = $statsTable->getColumn($this->valueInternalName)->getValues();
-        $overs = $statsTable->getColumn($this->overInternalName)->getValues();
+        $values = $statsTable->getColumn($this->numerator)->getValues();
+        $overs = $statsTable->getColumn($this->denominator)->getValues();
 
         foreach ($statsTable->getIndexes() as $index) {
             $value = $values[$index];
